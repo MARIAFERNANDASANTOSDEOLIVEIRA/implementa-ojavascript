2b1f4df2b84f109ff55560cde667cd91052595cb
MARIAFERNANDASANTOSDEOLIVEIRA <162495412+MARIAFERNANDASANTOSDEOLIVEIRA@users.noreply.github.com>
2024-04-11 17:09:30 -0300

    j

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const dataatual= new date();
+const dataatual = new date();
 let dataniver = prompt (data do próximo niver );
 dataniver = new Date (dataniver + "T23:59:59");
 let diasquefaltam = Math.floor((dataniver-dataatual)/86400000);
